c99ea608b2a0800030bd45868c8da17134670490
alex4o <mrabonin@gmail.com>
2016-11-04 18:44:03 +0200

    added newpages

---
+++ src/components/Art.vue
@@ -0,0 +1,37 @@
+<template>
+
+ 			<div id="pages">
+				<div class="page">
+
+
+				</div>
+			</div>
+</template>
+
+<script>
+export default {
+	name: 'art',
+
+	components: {
+		
+	},
+	mounted: function() {
+		// console.log(this)
+		// console.log(this.$emit)
+		this.$emit('bg', "red")
+	}
+}
+</script>
+
+<style scoped lang="stylus">
+
+#pages > .page 
+	display: block
+	background: pink
+	height: 60vh
+	width: 60vw
+	margin-top: 10vh
+	margin-left: 20vw
+	margin-right: 20vw
+
+</style>
